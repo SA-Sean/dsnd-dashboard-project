0761f47ba752e2943e112f500033c4409e7985e6
Sean Meyer <sean.meyer0@gmail.com>
2026-01-19 23:02:42 +0200

    add sql execution methods to sql_execution.py

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,26 +5,82 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
+cwd = Path(__file__).parent
+db_path = cwd / 'employee_events.db'
 
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
 class QueryMixin:
-    
+
+
     # Define a method named `pandas_query`
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(sql_query:str) -> pd.DataFrame:
+
+        """
+        Excutes and SQL query against a database connection and returns the result as a padas dataframe
+        
+        """
+
+        try:
+            # establish db connection
+            db_conn = connect(db_path)
+
+            # query database and return result as dataframe
+            sql_result = pd.read_sql_query(sql_query, db_conn)
+            
+            # close database connection
+            db_conn.close()
+        
+            return sql_result # return pandas dataframe with query result data
+
+        except Exception as e:
+            
+            # catch and show any errors that occure in the DB interaction
+            print(f"An error with the database interaction occurred: {e}")
+
+            return pd.DataFrame()   # return empty dataframe on failure
+        
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
+def query(sql_query:str):
+
+    """
+    Excutes and SQL query against a database connection and returns the result as a list of tuples
+
+    """
     
+    try:
+        # establish db connection
+        db_conn = connect(db_path)
+
+        # create cursor object to run the query
+        cursor = db_conn.cursor()
+
+        # execute the query
+        cursor.execute(sql_query)
+
+        # fetch all the results as a list of tuples
+        result = cursor.fetchall()
+
+        # close db connection
+        db_conn.close()
+
+        return result
+
+    except Exception as e:
+
+            # catch and show any errors that occure in the DB interaction
+            print(f"An error with the database interaction occurred: {e}")
+
+            return pd.DataFrame()   # return empty dataframe on failure
 
  
  # Leave this code unchanged
@@ -44,3 +100,5 @@ def query(func):
         return result
     
     return run_query
+
+
